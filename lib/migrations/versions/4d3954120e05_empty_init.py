901cbef09d525d7dec3c04fff73472ed7a887819
Diana Jordan <dianacjordan@gmail.com>
2023-04-17 00:23:43 -0400

    submit lab

---
+++ lib/migrations/versions/4d3954120e05_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4d3954120e05
+Revises: 
+Create Date: 2023-04-17 00:07:01.679870
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4d3954120e05'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
